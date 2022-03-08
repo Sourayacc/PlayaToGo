834053e394826e060cf5fe6d555f232cc73966bd
Antoine <camarasa.ltd@gmail.com>
2022-03-08 14:20:08 +0400

    Booking controller + Validates 3 model

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,53 @@
 class BookingsController < ApplicationController
+  before_action :set_booking, only: [:show, :edit, :update, :destroy]
+
   def index
     @bookings = Booking.all
   end
 
-  
+  def show
+    @booking = Booking.new
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @booking = Booking.new(booking_params)
+
+    if @booking.save
+      redirect_to @booking, notice: 'Your reservation has been successfully created.'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    if @booking.update(booking_params)
+      redirect_to @booking, notice: 'Your reservation has been successfully updated.'
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to bookings_url, notice: 'Your reservation was successfully destroyed.'
+  end
+
+
+private
+
+  def set_booking
+    @booking = Booking.find(params[:id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:starting_date, :ending_date)
+  end
 
 end
