752de3e00dc12d1d7226896fbfce36973158f056
Antoine <camarasa.ltd@gmail.com>
2022-03-08 13:00:23 +0400

    validates

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,7 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :beach
+
+  validates :starting_date, presence: true
+  validates :ending_date, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :bookings, dependent: :destroy
+  has_many :beaches, dependent: :destroy
+
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, presence: true, uniqueness: true
+  validates :encryptlastssword, presence: true
 end
