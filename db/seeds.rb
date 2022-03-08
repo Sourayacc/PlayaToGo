2ceb9110fe583f15014440cb8f8d85d7687bf96d
souraya cassam chenai <sourayacc@gmail.com>
2022-03-08 17:42:27 +0400

    seed et active storage

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,30 +5,48 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+
 Beach.destroy_all
-User.destroy.all
+User.destroy_all
 new_user = User.create!(first_name: "Antoine",
                         last_name: "Camarasa",
                         email: "antoine.camarasa@gmail.com",
-                        password: "antoine@camarasa86"
+                        password: "password"
                       )
-puts(new_user.first_name)
+file = URI.open('https://www.tropicalement-votre.com/dests/maurice/trou-aux-biches-plage-maurice.jpg')
 beach = Beach.create!(title: "Trou aux biches",
             description: "Jolie Plage ensoleillée, avec des filaos qui donnent de l'ombre, on peut y trouver des tortues de mer",
             address: "route royale, Trou au Biches",
             price: "Rs8000",
             user: new_user)
+
+beach.photo.attach(io: file, filename: 'trou-aux-biches-plage-maurice.jpg', content_type: 'image/jpg')
 puts(beach.title, beach.user.first_name)
+
+
+beach = Beach.create!(title: "Mont Choisi",
+  description: "Plage mignonne, eau bien transparence, on peut y ramasser des coquillages",
+  address: "route royale, Mont Choisi",
+  price: "Rs5000",
+  user: new_user)
+  puts(beach.title, beach.user.first_name)
 
 new_user = User.create!(first_name: "Souraya",
   last_name: "Cassam Chenai",
-  email: "souraya.cassam_chenai@gmail.com",
-  password: "souraya@"
+  email: "sourayacc@gmail.com",
+  password: "password"
 )
-puts(new_user.first_name)
-beach = Beach.create!(title: "Trou aux biches",
-description: "Jolie Plage ensoleillée, avec des filaos qui donnent de l'ombre, on peut y trouver des tortues de mer",
-address: "route royale, Trou au Biches",
-price: "Rs8000",
+beach = Beach.create!(title: "Tamarin Bay",
+description: "Jolie Plage plein d'ambiance, vendeur de boulettes et de glaçons râpés",
+address: "route royale, Tamarin",
+price: "Rs5000",
 user: new_user)
 puts(beach.title, beach.user.first_name)
+
+beach = Beach.create!(title: "Le Morne",
+  description: "Grande et Jolie plage, plein d'ombres avec les filao",
+  address: "route royale, Le Morne",
+  price: "Rs10000",
+  user: new_user)
+  puts(beach.title, beach.user.first_name)
